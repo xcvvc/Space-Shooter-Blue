bb0790f9b66a016a44a53080dcf179de07cb4e37
Lance Gold <x@xcvvc.com>
2025-02-19 14:05:57 -0800

    Created a scene with a player cube and script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // speed variable for Player movement
+    [SerializeField]
+    private float _speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //take the current position set to new position (0,0,0)
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move Vector 5 meters per second
+        transform.Translate(Vector3.right * _speed * Time.deltaTime);
+    }
+}
