119d8c37f2c6cc4c5c0afef86a47184412d86d78
Lance Gold <x@xcvvc.com>
2025-04-07 22:37:55 -0700

    Added Main Menu Title Screen

---
+++ Assets/Scripts/Main_Menu/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void LoadGame()
+    {
+        // load the GameScene
+        SceneManager.LoadScene(1); // main game scene
+    }
+}
